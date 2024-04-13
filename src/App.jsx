cdbafd838a7607c4caee786e70b557e2e6c5b582
devBuzz142 <devbuzz142@gmail.com>
2024-04-14 00:42:02 +0900

    feat: 기초구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "@emotion/styled";
+
+const AppSection = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  background-image: url("/src/assets/apecamp.jpg");
+  background-size: 1020px;
+  background-position-x: -50%;
+  background-position-y: -36px;
+
+  padding-top: 160px;
+  padding-bottom: 120px;
+  padding-left: 120px;
+  padding-right: 120px;
+
+  overflow-y: scroll;
+`;
+
+const MainSection = styled.main`
+  background-color: rgba(255, 255, 255);
+  width: 100%;
+  min-height: 360px;
+
+  border-radius: 20px;
+  border: 2px solid rgba(0, 0, 0, 0.1);
+
+  padding-top: 60px;
+  padding-bottom: 36px;
+  padding-left: 48px;
+  padding-right: 48px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppSection>
+      <MainSection>
+        <h1>학습하는 기계를 통해, 방황하는 인간을 탐구하는 정종관입니다.</h1>
+        <h2>반갑습니다</h2>
+        <div>hello</div>
+        <video src="/src/assets/arduino01.mp4" loop autoPlay width={720} />
+        <video src="/src/assets/arduino02.mp4" loop autoPlay width={360} />
+      </MainSection>
+    </AppSection>
+  );
 }
 
-export default App
+export default App;
---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,49 @@
+import { Global, css } from "@emotion/react";
+
+const GlobalStyle = () => {
+  return (
+    <Global
+      styles={css`
+        * {
+          position: relative;
+
+          margin: 0;
+          padding: 0;
+          border: none;
+
+          /* font-size: 28px; */
+
+          ::-webkit-scrollbar {
+            /* display: none; */
+          }
+        }
+
+        *,
+        ::after,
+        ::before {
+          box-sizing: border-box;
+        }
+
+        // 사실상 <html>
+        :root {
+          -webkit-tap-highlight-color: transparent;
+          -webkit-text-size-adjust: 100%;
+          text-size-adjust: 100%;
+          cursor: default;
+          line-height: 1.5;
+          overflow-wrap: break-word;
+          -moz-tab-size: 4;
+          tab-size: 4;
+        }
+
+        html,
+        body {
+          width: 100%;
+          height: 100%;
+        }
+      `}
+    />
+  );
+};
+
+export default GlobalStyle;
