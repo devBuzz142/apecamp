fc97b2f0033376a6c75abd46c750b7448737124e
devBuzz142 <devbuzz142@gmail.com>
2024-04-16 00:57:11 +0900

    feat: hamelin page

--- src/pages/CodeplayPage.jsx
+++ src/pages/CodeplayPage.jsx
@@ -5,8 +5,13 @@ const CodeplayPage = () => {
     <Page>
       <PageLeftSection>
         <h1>Codeplay</h1>
-        <p>Codeplay content goes here...</p>
-        <p>음악 생성 프로젝트 Codeplay</p>
+        <p>
+          네이버 부스트캠프에서 개발한 인공지능 음악 생성 웹 어플리케이션입니다.
+          4개월간 팀원들과 기획/자료조사/논문리딩/인공지능학습/웹개발을
+          수행하였습니다. 언어모델은 사용자가 입력한 프롬프트로부터 '장르',
+          '감정'과 같은 비계량적 요소를 추출합니다. GPT2에 약 2만2천개의 노래를
+          학습한 생성모델은 해당 요소에 어울리는 음악을 생성합니다.
+        </p>
         <div>
           <a href="https://codeplay-ai.netlify.app">
             https://codeplay-ai.netlify.app
@@ -19,7 +24,7 @@ const CodeplayPage = () => {
         </div>
       </PageLeftSection>
       <PageRightSection>
-        <img src="/src/assets/codeplay01.png" alt="Codeplay" width={720} />
+        <img src="/assets/codeplay01.png" alt="Codeplay" width={720} />
       </PageRightSection>
     </Page>
   );
--- src/pages/HamelinPage.jsx
+++ src/pages/HamelinPage.jsx
@@ -1,11 +1,7 @@
 import styled from "@emotion/styled";
 import Page, { PageLeftSection, PageRightSection } from "./Page";
-
-const XScrollBox = styled.div`
-  display: flex;
-  flex-wrap: nowrap;
-  overflow-x: auto;
-`;
+import { useState } from "react";
+import { Pagination, Stack } from "@mui/material";
 
 const hamelinImages = Array.from(
   { length: 32 },
@@ -20,26 +16,61 @@ const ImageWrapper = styled.div`
 
   border: 4px solid rgba(0, 0, 0, 0.8);
   box-shadow: 24;
+
+  max-width: 640px;
+`;
+
+const PaginationButtons = ({ page = 1, setPage }) => {
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Pagination count={32} page={page} onChange={handleChange} />
+    </Stack>
+  );
+};
+
+const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
 `;
 
 const HamelinPage = () => {
+  const [currentImage, setCurrentImage] = useState(1);
+
   return (
     <Page>
       <PageLeftSection>
         <h1>Hamelin</h1>
         <div>
-          하멜린을 하면서 생성 이미지 활용하였습니다. 이를 통해 --할 수
-          있었습니다.
+          ChatGPT의 등장에 인공지능을 연출한 공연에 활용해보기로 하였습니다.
+          이에 이미지 생성 인공지능인 DALL-E를 활용하여 이미지를 생성하였습니다.
+          창작자들과 극에서 주요하게 사용되는 개념어를 추출하였습니다.
+          탁상작업의 과정에서 자주 사용되는 단어나, 디자이너들이 작업을 함에
+          있어 핵심으로 여긴 개념어를 수합하였습니다. 이를 통해 얻은 [‘추상’,
+          ‘격리’, ‘거리두기’, ‘아동’, ‘성당’, ‘언론’, ‘흑백’] 등의 단어를
+          조합하여 프롬프팅한 이미지를 극 중에서 활용하였습니다. 본 과정을 통해
+          프롬프팅을 넘어, 인공지능의 원리에 대해 이해하고, 인공지능을 무대
+          장치화할 수 있는 기술적 능력을 습득하기 위해 인공지능 교육과정을
+          수강하였습니다.
         </div>
       </PageLeftSection>
       <PageRightSection>
-        <XScrollBox>
-          {hamelinImages.map((src, i) => (
-            <ImageWrapper key={i}>
-              <img key={i} src={src} width={660} />
-            </ImageWrapper>
-          ))}
-        </XScrollBox>
+        <ImageWrapper>
+          <img
+            src={hamelinImages[currentImage - 1]}
+            alt="hamelin"
+            style={{ maxWidth: "100%", maxHeight: "100%" }}
+          />
+        </ImageWrapper>
+        <PaginationWrapper>
+          <PaginationButtons page={currentImage} setPage={setCurrentImage} />
+        </PaginationWrapper>
       </PageRightSection>
     </Page>
   );
