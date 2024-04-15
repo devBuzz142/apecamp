829a03f41c1aa6c9b0b7333c65de8e2ec4c66713
devBuzz142 <devbuzz142@gmail.com>
2024-04-16 01:55:16 +0900

    feat: 구성 완

--- src/pages/LastPage.jsx
+++ src/pages/LastPage.jsx
@@ -9,11 +9,27 @@ const ImageWrapper = styled.div`
   height: 100%;
 `;
 
+const Header = styled.div`
+  // same as h1
+  font-size: 2rem;
+  font-weight: bold;
+
+  display: flex;
+`;
+
+const BlueWrapper = styled.p`
+  color: blue;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
 const LastPage = () => {
   return (
     <Page>
       <PageLeftSection>
-        <h1>감사합니다.</h1>
+        <Header>
+          적극적인 한 명의 <BlueWrapper>APE</BlueWrapper>가 되겠습니다.
+        </Header>
         <br />
         <div>
           {"<"}Codeplay{">"} 외에 기술을 예술분야에 적용시켜 본 경험이 아직
@@ -26,12 +42,13 @@ const LastPage = () => {
           사람들을 만나고, 공유하고 싶습니다.
         </div>
         <br />
-        <div>
-          본 과정을 통해 많은 사람들을 나누고, 성장하고 싶어 지원했습니다.
-          감사합니다.
-        </div>
+        <div>본 과정을 통해 많은 사람들을 나누고, 성장하고자 지원했습니다.</div>
       </PageLeftSection>
       <PageRightSection>
+        <br />
+        <br />
+        <br />
+        <br />
         <ImageWrapper>감사합니다.</ImageWrapper>
       </PageRightSection>
     </Page>
